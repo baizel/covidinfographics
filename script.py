1b20b19b9dba15dfd49395167a6b3a2de5df6ac8
baizel <baizelmathew@yahoo.co.in>
2020-04-21 21:15:00 +0100

    Fix path bug

--- script.py
+++ script.py
@@ -15,10 +15,14 @@ for dir in os.listdir("graphics/"):
                              }
                          }
                      ]}
+            path = "graphics"+"/"+dir+"/"+file
+            path.strip()
+            os.rename("graphics"+"/"+dir+"/"+file,path)
             entry["language"] = dir
             entry["graphics"][0]["info"]["name"] = "Overview"
-            entry["graphics"][0]["info"]["src"] = dir+"/"+file
+            entry["graphics"][0]["info"]["src"] = path
             data.append(entry)
+            print(path)
         else:
             print(file.split(".")[-1])
 print(json.dumps(data))
